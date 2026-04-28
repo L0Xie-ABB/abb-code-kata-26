d85f4d6e54457ead62d557613696b41705c59651
L0Xie-ABB <ziyu.xie@ch.abb.com>
2026-04-28 10:17:58 +0000

    Brie logic

--- cpp/src/GildedRose.cc
+++ cpp/src/GildedRose.cc
@@ -68,34 +68,37 @@ void GildedRose::updateItem(Item &item)
         {
             item.quality = item.quality + 1;
         }
-    }
 
-    if (item.name != "Sulfuras, Hand of Ragnaros")
-    {
-        item.sellIn = item.sellIn - 1;
-    }
-
-    if (item.sellIn < 0)
-    {
-        if (item.name != "Aged Brie")
+        if (item.name != "Sulfuras, Hand of Ragnaros")
         {
-            if (item.name != "Backstage passes to a TAFKAL80ETC concert")
+            item.sellIn = item.sellIn - 1;
+        }
+    
+        if (item.sellIn < 0)
+        {
+            if (item.name != "Aged Brie")
             {
-                if (item.quality > 0)
+                if (item.name != "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    if (item.name != "Sulfuras, Hand of Ragnaros")
+                    if (item.quality > 0)
                     {
-                        item.quality = item.quality - 1;
+                        if (item.name != "Sulfuras, Hand of Ragnaros")
+                        {
+                            item.quality = item.quality - 1;
+                        }
                     }
                 }
             }
-        }
-        else
-        {
-            if (item.quality < 50)
+            else
             {
-                item.quality = item.quality + 1;
+                if (item.quality < 50)
+                {
+                    item.quality = item.quality + 1;
+                }
             }
         }
+
+        return;
     }
+
 }
