7cc143a736f782012f6aeb370fdfe11af1ad37f3
L0Xie-ABB <ziyu.xie@ch.abb.com>
2026-04-28 08:46:42 +0000

    Sulfuras unit test

--- cpp/test/cpp_googletest_unittest/GildedRoseGoogletestUnitTests.cc
+++ cpp/test/cpp_googletest_unittest/GildedRoseGoogletestUnitTests.cc
@@ -4,8 +4,10 @@
 TEST(GildedRoseTest, Foo)
 {
     std::vector<Item> items;
-    items.push_back(Item("Foo", 0, 0));
+    items.push_back(Item("Sulfuras, Hand of Ragnaros", 1000, 80));
     GildedRose app(items);
     app.updateQuality();
-    EXPECT_EQ("fixme", app.items[0].name);
+    EXPECT_EQ("Sulfuras, Hand of Ragnaros", app.items[0].name);
+    EXPECT_EQ(1000, app.items[0].sellIn);
+    EXPECT_EQ(80, app.items[0].quality);
 }
