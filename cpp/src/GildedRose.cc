ebc26017f2d35c3ecb6be8b112f4af95ef045b55
L0Xie-ABB <ziyu.xie@ch.abb.com>
2026-04-28 10:20:32 +0000

    Brie cleanup

--- cpp/src/GildedRose.cc
+++ cpp/src/GildedRose.cc
@@ -24,32 +24,14 @@ void GildedRose::updateItem(Item &item)
             item.quality = item.quality + 1;
         }
 
-        if (item.name != "Sulfuras, Hand of Ragnaros")
-        {
-            item.sellIn = item.sellIn - 1;
-        }
+        item.sellIn = item.sellIn - 1;
 
         if (item.sellIn < 0)
         {
-            if (item.name != "Aged Brie")
+
+            if (item.quality < 50)
             {
-                if (item.name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (item.quality > 0)
-                    {
-                        if (item.name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            item.quality = item.quality - 1;
-                        }
-                    }
-                }
-            }
-            else
-            {
-                if (item.quality < 50)
-                {
-                    item.quality = item.quality + 1;
-                }
+                item.quality = item.quality + 1;
             }
         }
 
