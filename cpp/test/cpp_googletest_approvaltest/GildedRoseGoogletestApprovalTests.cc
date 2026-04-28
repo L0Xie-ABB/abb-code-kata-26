0278283e4737780e77638f82ba544dfd5adb1d62
L0Xie-ABB <ziyu.xie@ch.abb.com>
2026-04-28 08:43:33 +0000

    Combination tests golden reference

--- cpp/test/cpp_googletest_approvaltest/GildedRoseGoogletestApprovalTests.cc
+++ cpp/test/cpp_googletest_approvaltest/GildedRoseGoogletestApprovalTests.cc
@@ -2,6 +2,7 @@
 #include <ApprovalTests.hpp>
 
 #include "GildedRose.h"
+#include <ranges>
 
 std::ostream &operator<<(std::ostream &os, const Item &obj)
 {
@@ -9,11 +10,93 @@ std::ostream &operator<<(std::ostream &os, const Item &obj)
               << ", quality: " << obj.quality;
 }
 
-TEST(GildedRoseApprovalTests, VerifyCombinations)
+TEST(GildedRoseApprovalTests, VerifyFoo)
 {
     std::vector<std::string> names{"Foo"};
-    std::vector<int> sellIns{1};
-    std::vector<int> qualities{1};
+    std::vector<int> sellIns{};
+    for (int i = -10; i < 10; i++)
+    {
+        sellIns.push_back(i);
+    }
+    std::vector<int> qualities{};
+    for (int i = 0; i < 51; i++)
+    {
+        qualities.push_back(i);
+    }
+
+    auto f = [](std::string name, int sellIn, int quality)
+    {
+        std::vector<Item> items = {Item(name, sellIn, quality)};
+        GildedRose app(items);
+        app.updateQuality();
+        return items[0];
+    };
+
+    ApprovalTests::CombinationApprovals::verifyAllCombinations(f, names, sellIns,
+                                                               qualities);
+}
+
+TEST(GildedRoseApprovalTests, VerifySulfuras)
+{
+    std::vector<std::string> names{"Sulfuras, Hand of Ragnaros"};
+    std::vector<int> sellIns{};
+    for (int i = -10; i < 10; i++)
+    {
+        sellIns.push_back(i);
+    }
+    std::vector<int> qualities{80};
+
+    auto f = [](std::string name, int sellIn, int quality)
+    {
+        std::vector<Item> items = {Item(name, sellIn, quality)};
+        GildedRose app(items);
+        app.updateQuality();
+        return items[0];
+    };
+
+    ApprovalTests::CombinationApprovals::verifyAllCombinations(f, names, sellIns,
+                                                               qualities);
+}
+
+TEST(GildedRoseApprovalTests, VerifyBrie)
+{
+    std::vector<std::string> names{"Aged Brie"};
+    std::vector<int> sellIns{};
+    for (int i = -10; i < 10; i++)
+    {
+        sellIns.push_back(i);
+    }
+    std::vector<int> qualities{};
+    for (int i = 0; i < 51; i++)
+    {
+        qualities.push_back(i);
+    }
+
+    auto f = [](std::string name, int sellIn, int quality)
+    {
+        std::vector<Item> items = {Item(name, sellIn, quality)};
+        GildedRose app(items);
+        app.updateQuality();
+        return items[0];
+    };
+
+    ApprovalTests::CombinationApprovals::verifyAllCombinations(f, names, sellIns,
+                                                               qualities);
+}
+
+TEST(GildedRoseApprovalTests, VerifyPass)
+{
+    std::vector<std::string> names{"Backstage passes to a TAFKAL80ETC concert"};
+    std::vector<int> sellIns{};
+    for (int i = -5; i < 15; i++)
+    {
+        sellIns.push_back(i);
+    }
+    std::vector<int> qualities{};
+    for (int i = 0; i < 51; i++)
+    {
+        qualities.push_back(i);
+    }
 
     auto f = [](std::string name, int sellIn, int quality)
     {
