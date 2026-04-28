cccce73b9d005432fc606ab31f9be3b31f2803c0
L0Xie-ABB <ziyu.xie@ch.abb.com>
2026-04-28 09:50:55 +0000

    Backstage quality seperation

--- cpp/src/GildedRose.cc
+++ cpp/src/GildedRose.cc
@@ -23,30 +23,30 @@ void GildedRose::updateItem(Item &item)
             item.quality = item.quality - 1;
         }
     }
+    else if (item.name == "Backstage passes to a TAFKAL80ETC concert")
+    {
+        item.quality = item.quality + 1;
+
+        if (item.sellIn < 11)
+        {
+            item.quality = item.quality + 1;
+        }
+
+        if (item.sellIn < 6)
+        {
+            item.quality = item.quality + 1;
+        }
+
+        if (item.quality > 50)
+        {
+            item.quality = 50;
+        }
+    }
     else
     {
         if (item.quality < 50)
         {
             item.quality = item.quality + 1;
-
-            if (item.name == "Backstage passes to a TAFKAL80ETC concert")
-            {
-                if (item.sellIn < 11)
-                {
-                    if (item.quality < 50)
-                    {
-                        item.quality = item.quality + 1;
-                    }
-                }
-
-                if (item.sellIn < 6)
-                {
-                    if (item.quality < 50)
-                    {
-                        item.quality = item.quality + 1;
-                    }
-                }
-            }
         }
     }
 
