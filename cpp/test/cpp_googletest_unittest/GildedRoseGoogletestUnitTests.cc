0b506c20d98d32c19edd9e2f0273b4b1b9968faa
L0Xie-ABB <ziyu.xie@ch.abb.com>
2026-04-28 08:50:46 +0000

    Special item unit tests

--- cpp/test/cpp_googletest_unittest/GildedRoseGoogletestUnitTests.cc
+++ cpp/test/cpp_googletest_unittest/GildedRoseGoogletestUnitTests.cc
@@ -11,3 +11,47 @@ TEST(GildedRoseTest, Foo)
     EXPECT_EQ(1000, app.items[0].sellIn);
     EXPECT_EQ(80, app.items[0].quality);
 }
+
+TEST(GildedRoseTest, PassTenPlus)
+{
+    std::vector<Item> items;
+    items.push_back(Item("Backstage passes to a TAFKAL80ETC concert", 15, 10));
+    GildedRose app(items);
+    app.updateQuality();
+    EXPECT_EQ("Backstage passes to a TAFKAL80ETC concert", app.items[0].name);
+    EXPECT_EQ(14, app.items[0].sellIn);
+    EXPECT_EQ(11, app.items[0].quality);
+}
+
+TEST(GildedRoseTest, PassTenMinus)
+{
+    std::vector<Item> items;
+    items.push_back(Item("Backstage passes to a TAFKAL80ETC concert", 9, 10));
+    GildedRose app(items);
+    app.updateQuality();
+    EXPECT_EQ("Backstage passes to a TAFKAL80ETC concert", app.items[0].name);
+    EXPECT_EQ(8, app.items[0].sellIn);
+    EXPECT_EQ(12, app.items[0].quality);
+}
+
+TEST(GildedRoseTest, PassFiveMinus)
+{
+    std::vector<Item> items;
+    items.push_back(Item("Backstage passes to a TAFKAL80ETC concert", 4, 10));
+    GildedRose app(items);
+    app.updateQuality();
+    EXPECT_EQ("Backstage passes to a TAFKAL80ETC concert", app.items[0].name);
+    EXPECT_EQ(3, app.items[0].sellIn);
+    EXPECT_EQ(13, app.items[0].quality);
+}
+
+TEST(GildedRoseTest, PassPassed)
+{
+    std::vector<Item> items;
+    items.push_back(Item("Backstage passes to a TAFKAL80ETC concert", 0, 10));
+    GildedRose app(items);
+    app.updateQuality();
+    EXPECT_EQ("Backstage passes to a TAFKAL80ETC concert", app.items[0].name);
+    EXPECT_EQ(-1, app.items[0].sellIn);
+    EXPECT_EQ(0, app.items[0].quality);
+}
