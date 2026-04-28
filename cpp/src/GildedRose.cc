fd6b655f5f95530111f486aee7bedcf3a239b28b
L0Xie-ABB <ziyu.xie@ch.abb.com>
2026-04-28 10:19:23 +0000

    Else cleanup

--- cpp/src/GildedRose.cc
+++ cpp/src/GildedRose.cc
@@ -16,18 +16,43 @@ void GildedRose::updateQuality()
 
 void GildedRose::updateItem(Item &item)
 {
-    if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" && item.name != "Sulfuras, Hand of Ragnaros")
+    if (item.name == "Aged Brie")
     {
-        if (item.quality > 0)
+
+        if (item.quality < 50)
         {
-            item.quality = item.quality - 1;
+            item.quality = item.quality + 1;
         }
 
-        item.sellIn--;
-        if (item.sellIn < 0 && item.quality > 0)
+        if (item.name != "Sulfuras, Hand of Ragnaros")
         {
-            item.quality = item.quality - 1;
+            item.sellIn = item.sellIn - 1;
         }
+
+        if (item.sellIn < 0)
+        {
+            if (item.name != "Aged Brie")
+            {
+                if (item.name != "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (item.quality > 0)
+                    {
+                        if (item.name != "Sulfuras, Hand of Ragnaros")
+                        {
+                            item.quality = item.quality - 1;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (item.quality < 50)
+                {
+                    item.quality = item.quality + 1;
+                }
+            }
+        }
+
         return;
     }
     else if (item.name == "Backstage passes to a TAFKAL80ETC concert")
@@ -64,41 +89,18 @@ void GildedRose::updateItem(Item &item)
     }
     else
     {
-        if (item.quality < 50)
+        if (item.quality > 0)
         {
-            item.quality = item.quality + 1;
+            item.quality = item.quality - 1;
         }
 
-        if (item.name != "Sulfuras, Hand of Ragnaros")
+        item.sellIn--;
+
+        if (item.sellIn < 0 && item.quality > 0)
         {
-            item.sellIn = item.sellIn - 1;
-        }
-    
-        if (item.sellIn < 0)
-        {
-            if (item.name != "Aged Brie")
-            {
-                if (item.name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (item.quality > 0)
-                    {
-                        if (item.name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            item.quality = item.quality - 1;
-                        }
-                    }
-                }
-            }
-            else
-            {
-                if (item.quality < 50)
-                {
-                    item.quality = item.quality + 1;
-                }
-            }
+            item.quality = item.quality - 1;
         }
 
         return;
     }
-
 }
